de516f38f4ac4eb0bd8565aad37fd68c86ed160c
cms02 <minoui1234@gmail.com>
2022-01-07 16:57:57 +0900

    feat: 스프링부트 기본로그인 + OAuth2.0로그인(구글, 페이스북, 네이버)

--- src/main/java/com/cms/security1/config/SecurityConfig.java
+++ src/main/java/com/cms/security1/config/SecurityConfig.java
@@ -15,19 +15,21 @@ import com.cms.security1.config.oauth.PrincipalOauth2UserService;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity	//스프링 시큐리티 필터(SecurityConfig)가 스프링 필터체인에 등록이 됨.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)	//secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+
 	private final PrincipalOauth2UserService principalOauth2UserService;
 	
+//	@Bean
+//	public BCryptPasswordEncoder encodePwd() {
+//		return new BCryptPasswordEncoder();
+//	}
+	
+	
 	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
-	@Bean
-	public BCryptPasswordEncoder encodePwd() {
-		return new BCryptPasswordEncoder();
-	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -37,18 +39,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
 			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 			.anyRequest().permitAll()
-			.and()
+		.and()
 			.formLogin()
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login") //login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줍니다.
 			.defaultSuccessUrl("/")
-			.and()
+		.and()
 			.oauth2Login()
 			.loginPage("/loginForm")
 			.userInfoEndpoint()
-			.userService(principalOauth2UserService)
-			
-			; 
+			.userService(principalOauth2UserService); 
 		//구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드X (엑세스토큰 + 사용자프로필정보 O)
 	}
 
--- src/main/java/com/cms/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cms/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.cms.security1.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -8,6 +10,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.cms.security1.config.auth.PrincipalDetails;
+import com.cms.security1.config.oauth.provider.FacebookUserInfo;
+import com.cms.security1.config.oauth.provider.GoogleUserInfo;
+import com.cms.security1.config.oauth.provider.NaverUserInfo;
+import com.cms.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cms.security1.model.User;
 import com.cms.security1.repository.UserRepository;
 
@@ -18,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 	
-	private final BCryptPasswordEncoder bBCryptPasswordEncoder;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	private final UserRepository userRepository;
 	
@@ -32,16 +38,40 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 		//userRequest 정보 -> loadUser 함수 -> 구글로부터 회원프로필을 받아준다.
 		System.out.println("getAttributes: " + oAuth2User.getAttributes());
 		
-		String provider 	= userRequest.getClientRegistration().getClientId(); //google
-		String providerId	= oAuth2User.getAttribute("sub");
+		//회원가입을 강제로 진행해볼 예정
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청");
+			oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+			System.out.println("페이스북 로그인 요청");
+			oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			System.out.println("네이버 로그인 요청");
+			oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+		}
+		else {
+			System.out.println("우리는 구글과 페이스북과 네이버만 지원합니다.");
+		}
+			
+//		String provider 	= userRequest.getClientRegistration().getRegistrationId(); //google
+		String provider 	= oAuth2UserInfo.getProvider();
+		
+//		String providerId	= oAuth2User.getAttribute("sub");
+		String providerId	= oAuth2UserInfo.getProviderId();
+		
 		String username		= provider + "_" + providerId;	//google_109125453223412
-		String password 	= bBCryptPasswordEncoder.encode("겟인데어");
-		String email		= oAuth2User.getAttribute("email");
+		
+		String password 	= bCryptPasswordEncoder.encode("겟인데어");
+		
+//		String email		= oAuth2User.getAttribute("email");
+		String email		= oAuth2UserInfo.getEmail();
 		String role			= "ROLE_USER";
 		
 		User userEntity 	= userRepository.findByUsername(username);
 		
 		if(userEntity == null) {
+			System.out.println("OAuth 로그인이 최초입니다.");
 			userEntity = User.builder()
 					.username(username)
 					.password(password)
@@ -51,9 +81,10 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 					.providerId(providerId)
 					.build();
 			userRepository.save(userEntity);
+		}else {
+			System.out.println("로그인을 이미 한적이 있습니다.당신은 자동회원가입이 되어있습니다.");
 		}
 		
-		//회원가입을 강제로 진행해볼 예정
 		return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
 	}
 
